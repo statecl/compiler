e2fa8238cdba7ddd5c5a530dd9b8df2383823ee5
Ian Torres <iantorres@outlook.com>
2026-06-20 13:28:04 -0400

    WIP

--- scripts/build-re2.sh
+++ scripts/build-re2.sh
@@ -22,15 +22,6 @@ elif [ "$BUILD_VARIANT" = "Debug" ]; then
     SHARED=ON
 fi
 
-SANITIZER_CMAKE=()
-if [ "$SANITIZER" != "off" ]; then
-    SANITIZER_CMAKE+=(
-        -DCMAKE_CXX_FLAGS="$SANITIZER_FLAGS -g -O1"
-        -DCMAKE_EXE_LINKER_FLAGS="$SANITIZER_FLAGS"
-        -DCMAKE_SHARED_LINKER_FLAGS="$SANITIZER_FLAGS"
-    )
-fi
-
 # Build Abseil (RE2 dependency) - without sanitizer flags to avoid
 # constexpr issues in Abseil with GCC + UBSan (hash_policy_traits.h)
 ABSEIL_BUILD_TYPE="$CMAKE_BUILD_TYPE"
@@ -50,7 +41,8 @@ cmake --build build --target install --parallel 2
 cd /
 rm -rf /tmp/abseil
 
-# Build RE2 (with sanitizer flags if requested)
+# Build RE2 (without sanitizer flags to avoid
+# constexpr issues in Abseil with GCC + UBSan (hash_policy_traits.h))
 git clone --depth 1 --branch "$RE2_VERSION" https://github.com/google/re2.git /tmp/re2
 cd /tmp/re2
 
@@ -58,7 +50,6 @@ CMAKE_ARGS=(
     -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE"
     -DRE2_BUILD_TESTING=OFF
     -DBUILD_SHARED_LIBS="$SHARED"
-    "${SANITIZER_CMAKE[@]}"
 )
 
 cmake -S . -B build "${CMAKE_ARGS[@]}"
